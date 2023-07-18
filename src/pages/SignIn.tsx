845eb0b743eff75947e15d27e36f104e6f567031
davidbarbi3r <davidbarbi3r@gmail.com>
2023-07-18 08:31:45 +0200

    :feat setup uncontroled SignIn form

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,28 +1,37 @@
-import { Link } from "react-router-dom";
-
 export default function SignIn() {
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const form = event.currentTarget;
+
+        const dataForm = new FormData(form);
+        const formJson = Object.fromEntries(dataForm.entries());
+
+        console.log(formJson);
+    };
+
     return (
         <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-        <form>
+        <form onSubmit={handleSubmit} method="post">
           <div className="input-wrapper">
             <label htmlFor="username">
                 Username
             </label>
-            <input type="text" id="username" />
+            <input type="text" name="username" />
           </div>
           <div className="input-wrapper">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input type="password" name="password" />
           </div>
           <div className="input-remember">
-            <input type="checkbox" id="remember-me" />
+            <input type="checkbox" name="remember-me" />
             <label htmlFor="remember-me">
                 Remember me
             </label>
           </div>
-          <Link to="/user-profile" className="sign-in-button">Sign In</Link>
+          <button type="submit" className="sign-in-button">Sign In</button>
         </form>
       </section>
     );
